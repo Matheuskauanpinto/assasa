85399f9430ba5840166959bddd65846d9eb28d4b
Matheus Kauan pinto <110540048+Matheuskauanpinto@users.noreply.github.com>
2024-03-04 16:34:48 -0300

    a

--- script.js
+++
@@ -1,5 +0,0 @@
-function openEnvelope() {
-    document.querySelector('.open-button').style.display = 'none';
-    document.querySelector('.hearts').classList.remove('hidden');
-    document.querySelector('.message').classList.remove('hidden');
-}
