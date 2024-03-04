1a2bf72e3ca6205dced5a6f97c8e600ea1455022
Matheus Kauan pinto <110540048+Matheuskauanpinto@users.noreply.github.com>
2024-03-04 16:31:40 -0300

    A

---
+++ script.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+    document.querySelector('.open-button').style.display = 'none';
+    document.querySelector('.hearts').classList.remove('hidden');
+    document.querySelector('.message').classList.remove('hidden');
+}
